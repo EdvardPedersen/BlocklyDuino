349865da629517cc9c4f4331004f3df3d869b5ba
hwallann <hwallann@gmail.com>
2019-08-31 16:27:10 +0200

    Fixup of Utility class
    Remove empty method from AirBit utility class - Fix

--- air-bit/refactored_code/refactor2/AirBitUtilsClass.h
+++ air-bit/refactored_code/refactor2/AirBitUtilsClass.h
@@ -10,9 +10,6 @@ using namespace airbit;
 
 class AirBitUtilsClass {       // The class
   public:             // Access specifier
-    void PrintDebug(AirBitDateTimeClass airTime, double lat, double lng,
-      float pm10, float pm25, float humidity, float temperature );
-
     void PrintReadingsToSd(File file, AirBitDateTimeClass airTime, double lat, double lng,
       float pm10, float pm25, float humidity, float temperature );
 
